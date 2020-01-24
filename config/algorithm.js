fa67154780b1df426fd98086d0cd2c8634cd3d02
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2020-01-24 22:14:38 +0100

    Added stop score.

--- config/algorithm.js
+++ config/algorithm.js
@@ -1,3 +1,9 @@
+export const elitism = 0.01;
+export const generationLimit = 100;
+export const mutationAggresivity = 0.5;
+export const populationSize = 250;
+export const stopScore = 100;
+
 export const evaluatorWeights = {
     commonNotConsecutiveWeekdays: 50.0,
     personalNotAvailableWeekdays: 50.0
