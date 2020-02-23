4f1ce3a42ee616587bf47faecbe75396e17c853f
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2020-02-23 21:00:47 +0100

    Changed to tournament selection.

--- config/algorithm.js
+++ config/algorithm.js
@@ -1,27 +1,29 @@
-export const elitism = 0.05;
-export const generationLimit = 100;
+export const elitism = 0.01;
+export const generationLimit = 1000;
 export const mutationProbability = 0.5;
 export const mutationAggresivity = 0.5;
 export const populationSize = 1000;
 export const stopScore = 100;
+export const tournamentSelectionRatio = 0.65;
 export const islandOptions = {
     islands: 3,
     migration: 0.1,
-    epoch: 10
+    epoch: 20
 };
 
 export const offsetRatio = 0.75;
 
 export const evaluatorWeights = {
-    commonNotConsecutiveWeekdays: 31.969,
-    personalNotAvailableWeekdays: 31.969,
-    personalNotAvailableDates: 31.969,
-    avoidDoublets: 3.197,
-    personalDesiredDutyDates: 0.32,
-    personalDutiesDistribution: 0.16,
-    totalDutyDaysBalance: 0.096,
-    personalDesiredFreeDates: 0.096,
-    weekendDaysBalance: 0.096,
-    isolatedWeekendDaysBalance: 0.064,
-    maxPossibleFreeWeekends: 0.064
+    commonNotConsecutiveWeekdays: 32.137,
+    personalNotAvailableWeekdays: 32.137,
+    personalNotAvailableDates: 32.137,
+    avoidDoublets: 1.339,
+    personalDesiredDutyDates: 0.134,
+    personalDutiesDistribution: 0.134,
+    totalDutyDaysBalance: 1.339,
+    personalDesiredFreeDates: 0.268,
+    weekendDaysBalance: 0.107,
+    freeWeekendsBalance: 0.08,
+    isolatedWeekendDaysBalance: 0.054,
+    maxPossibleFreeWeekends: 0.134
 };
